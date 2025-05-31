b8986b4b45a494e03f42c17f1e6312198bfe0741
Santi Roldán <supersantiprogrammer@gmail.com>
2025-06-01 01:22:56 +0200

    feat: Implement Reader class for dependency injection and functional composition

---
+++ src/reader/reader.ts
@@ -0,0 +1,26 @@
+import {Monad} from "@/shared/monad";
+
+export class Reader<Env, A> implements Monad<A> {
+    private constructor(private readonly runFn: (env: Env) => A) {
+    }
+
+    public static of<Env, A>(value: A): Reader<Env, A> {
+        return new Reader(() => value);
+    }
+
+    public static ask<Env>(): Reader<Env, Env> {
+        return new Reader((env) => env);
+    }
+
+    public run(env: Env): A {
+        return this.runFn(env);
+    }
+
+    public map<B>(f: (a: A) => B): Reader<Env, B> {
+        return new Reader((env) => f(this.runFn(env)));
+    }
+
+    public flatMap<B>(f: (a: A) => Reader<Env, B>): Reader<Env, B> {
+        return new Reader((env) => f(this.runFn(env)).run(env));
+    }
+}
